6a8b7cebcd60fab55907f40561e8cb847beb0add
yang.geng.chen <yang.gengchen@sunpig.club>
2021-07-19 18:03:19 +0800

    ``

---
+++ src/main/java/com/yanggc/swd4offer/Solution001.java
@@ -0,0 +1,19 @@
+package com.yanggc.swd4offer;
+
+/**
+ * Description:
+ *
+ * @Author: Yanggc
+ * DateTime:07-19 17:43
+ */
+public class Solution001 {
+    public static int fib(int n) {
+        if(n == 0){return 0;}
+        if(n == 1){ return 1;}
+        return (fib(n-1) + fib(n-2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(100));
+    }
+}
