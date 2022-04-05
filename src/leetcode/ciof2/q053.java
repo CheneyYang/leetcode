c680c5feebea8b7c4909f43064ac4e79ac757791
yanggc <younggc@foxmail.com>
2022-04-06 01:20:17 +0800

    q053

---
+++ src/leetcode/ciof2/q053.java
@@ -0,0 +1,43 @@
+package leetcode.ciof2;
+
+/**
+ * 统计一个数字在排序数组中出现的次数。
+ *
+ * 示例 1:
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: 2
+ *
+ * 示例 2:
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: 0
+ *
+ */
+
+
+public class q053 {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                if (nums[low] == nums[high]) {
+                    return high - low + 1;
+                }
+                if (nums[low] < target) {
+                    low++;
+                }
+                if (nums[high] > target) {
+                    high--;
+                }
+            }
+
+        }
+        return 0;
+    }
+}
