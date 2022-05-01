bb304fa68b7c011c19012c6780269ec6f42ea1fe
yanggc <younggc@foxmail.com>
2022-05-01 23:11:43 +0800

    树的子结构

---
+++ src/leetcode/ciof2/q026.java
@@ -0,0 +1,91 @@
+package leetcode.ciof2;
+
+/**
+ * 树的子结构
+ * Description:
+ * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ * <p>
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ * <p>
+ * 例如:
+ * 给定的树 A:
+ * <p>
+ * 3
+ * / \
+ * 4  5
+ * / \
+ * 1  2
+ * 给定的树 B：
+ * <p>
+ * 4
+ * /
+ * 1
+ * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：A = [1,2,3], B = [3,1]
+ * 输出：false
+ * 示例 2：
+ * <p>
+ * 输入：A = [3,4,5,1,2], B = [4,1]
+ * 输出：true
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 10000
+ *
+ * @Author: YangGC
+ * DateTime: 04-30
+ */
+public class q026 {
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+
+
+    boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) {
+            return true;
+        }
+        if (A == null || A.val != B.val) {
+            return false;
+        }
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+    }
+}
