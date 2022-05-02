db593746bc6398bd0ce33986d68768df5c7d134b
yanggc <younggc@foxmail.com>
2022-05-02 23:37:48 +0800

    二叉树中和为某一值的路径

---
+++ src/leetcode/ciof2/q034.java
@@ -0,0 +1,92 @@
+package leetcode.ciof2;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 二叉树中和为某一值的路径
+ * Description:
+ * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *  输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * 输出：[[5,4,11,2],[5,8,4,5]]
+ *
+ *输入：root = [1,2,3], targetSum = 5
+ * 输出：[]
+ * 示例 3：
+ *输入：root = [1,2], targetSum = 0
+ * 输出：[]
+ *
+ * 提示：
+ *
+ * 树中节点总数在范围 [0, 5000] 内
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ *
+ * @Author: YangGC
+ * DateTime: 05-02
+ */
+public class q034 {
+
+    List<List<Integer>> res = new LinkedList<>();
+    Deque<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+        recur(root, target);
+        return res;
+    }
+
+
+    void recur(TreeNode root, int target) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null) {
+            res.add(new LinkedList<>(path));
+        }
+        recur(root.left, target);
+        recur(root.right,target);
+        path.removeLast();
+    }
+
+    private class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+    }
+}
