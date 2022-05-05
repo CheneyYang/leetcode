560ecdffc51f2d3035696529776b5c219b8e8b74
yanggc <younggc@foxmail.com>
2022-05-05 22:33:32 +0800

    遍历矩阵路径

---
+++ src/leetcode/ciof2/q012.java
@@ -0,0 +1,65 @@
+package leetcode.ciof2;
+
+/**
+ * 矩阵中的路径
+ * Description:
+ * 给定一个m x n 二维字符网格board 和一个字符串单词word 。如果word 存在于网格中，返回 true ；否则，返回 false 。
+ * <p>
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+ * <p>
+ * <p>
+ * 例如，在下面的 3×4 的矩阵中包含单词 "ABCCED"（单词中的字母已标出）。
+ * <p>
+ * 示例 1：
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * 输入：board = [["a","b"],["c","d"]], word = "abcd"
+ * 输出：false
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= board.length <= 200
+ * 1 <= board[i].length <= 200
+ * board 和 word 仅由大小写英文字母组成
+ *
+ * @Author: YangGC
+ * DateTime: 05-05
+ */
+public class q012 {
+
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) {
+            return false;
+        }
+        if (k == word.length - 1) {
+            return true;
+        }
+        board[i][j] = '\0';
+
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = word[k];
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("....");
+    }
+
+
+}
