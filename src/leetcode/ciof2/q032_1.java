1fcaf6edda1c014f9ced13575eee11c021c60018
yanggc <younggc@foxmail.com>
2022-05-11 20:57:30 +0800

    从上到下打印二叉树

---
+++ src/leetcode/ciof2/q032_1.java
@@ -0,0 +1,86 @@
+package leetcode.ciof2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 从上到下打印二叉树
+ * Description:
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ * 返回：
+ * [3,9,20,15,7]
+ * @Author: YangGC
+ * DateTime: 05-11
+ */
+public class q032_1 {
+
+    public int[] levelOrder(TreeNode root) {
+        if(root == null){
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<>() {{
+            add(root);
+        }};
+        List<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            ans.add(node.val);
+            if(node.left != null){
+                queue.add(node.left);
+            }
+            if(node.right != null){
+                queue.add(node.right);
+            }
+        }
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+    }
+}
