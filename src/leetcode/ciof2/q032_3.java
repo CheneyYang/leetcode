e81aa139cbb5c6a115009ac1df6d40e64cc75b26
yanggc <younggc@foxmail.com>
2022-05-12 11:40:27 +0800

    从上到下打印二叉树 奇数层和偶数层不一样

---
+++ src/leetcode/ciof2/q032_3.java
@@ -0,0 +1,95 @@
+package leetcode.ciof2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 从上到下打印二叉树 III
+ * Description:
+ * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * [
+ * [3],
+ * [20,9],
+ * [15,7]
+ * ]
+ *
+ * @Author: YangGC
+ * DateTime: 05-12
+ */
+public class q032_3 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> temp = new LinkedList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                //偶数层->队列头部
+                if (res.size() % 2 == 0) {
+                    temp.addLast(node.val);
+                } else {
+                    //奇数层->队列尾部
+                    temp.addFirst(node.val);
+                }
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+
+    private class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+    }
+}
