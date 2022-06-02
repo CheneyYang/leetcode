60c361436eb5ba0086324aacb47e8ea13254c3bd
yanggc <younggc@foxmail.com>
2022-06-02 22:22:16 +0800

    数据流中的中位数

---
+++ src/leetcode/ciof2/q041.java
@@ -0,0 +1,64 @@
+package leetcode.ciof2;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 数据流中的中位数
+ * Description:
+ * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
+ * <p>
+ * 例如，
+ * <p>
+ * [2,3,4] 的中位数是 3
+ * <p>
+ * [2,3] 的中位数是 (2 + 3) / 2 = 2.5
+ * <p>
+ * 设计一个支持以下两种操作的数据结构：
+ * <p>
+ * void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+ * double findMedian() - 返回目前所有元素的中位数。
+ * <p>
+ * 示例 1：
+ * 输入：
+ * ["MedianFinder","addNum","addNum","findMedian","addNum","findMedian"]
+ * [[],[1],[2],[],[3],[]]
+ * 输出：[null,null,null,1.50000,null,2.00000]
+ * <p>
+ * 示例 2：
+ * 输入：
+ * ["MedianFinder","addNum","findMedian","addNum","findMedian"]
+ * [[],[2],[],[3],[]]
+ * 输出：[null,null,2.00000,null,2.50000]
+ *
+ * @Author: YangGC
+ * DateTime: 06-02
+ */
+public class q041 {
+
+    Queue<Integer> A, B;
+
+    /**
+     * initialize your data structure here.
+     */
+    public q041() {
+        //小顶堆,保存较大的一半
+        A = new PriorityQueue<>();
+        //大顶堆,保存较小的一半
+        B = new PriorityQueue<>((x, y) -> (y - x));
+    }
+
+    public void addNum(int num) {
+        if (A.size() != B.size()) {
+            A.add(num);
+            B.add(A.poll());
+        } else {
+            B.add(num);
+            A.add(B.poll());
+        }
+    }
+
+    public double findMedian() {
+        return A.size() != B.size() ? A.peek() : (A.peek() + B.peek()) / 2.0;
+    }
+}
