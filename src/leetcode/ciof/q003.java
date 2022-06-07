c460748dd139a1a452be02c172dccbde3a8dea44
yanggc <younggc@foxmail.com>
2022-06-07 22:30:26 +0800

    ying

---
+++ src/leetcode/ciof/q003.java
@@ -0,0 +1,46 @@
+package leetcode.ciof;
+
+/**
+ * 前 n 个数字二进制中 1 的个数
+ * Description:
+ * 给定一个非负整数 n ，请计算 0 到 n 之间的每个数字的二进制表示中 1 的个数，并输出一个数组。
+ * <p>
+ * 示例 1:
+ * 输入: n = 2
+ * 输出: [0,1,1]
+ * 解释:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * <p>
+ * 示例 2:
+ * 输入: n = 5
+ * 输出: [0,1,1,2,1,2]
+ * 解释:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
+ *
+ * @Author: YangGC
+ * DateTime: 06-07
+ */
+public class q003 {
+    public int[] countBits(int n) {
+        int[] bits = new int[n + 1];
+        int highBit = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                highBit = i;
+            }
+            bits[i] = bits[i - highBit] + 1;
+        }
+        return bits;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
