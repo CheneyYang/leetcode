7addf8b5ccd61927125504028a385d41df01f7f6
yanggc <younggc@foxmail.com>
2022-06-11 20:41:18 +0800

    排序数组中两个数字之和

---
+++ src/leetcode/ciof/q006.java
@@ -0,0 +1,35 @@
+package leetcode.ciof;
+
+/**
+ * 排序数组中两个数字之和
+ * Description:
+ * 给定一个已按照 升序排列 的整数数组numbers ，请你从数组中找出两个数满足相加之和等于目标数target 。
+ * <p>
+ * 函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 0开始计数 ，所以答案数组应当满足 0<= answer[0] < answer[1] <numbers.length。
+ * <p>
+ * 假设数组中存在且只存在一对符合条件的数字，同时一个数字不能使用两次。
+ * 示例 1：
+ * 输入：numbers = [1,2,4,6,10], target = 8
+ * 输出：[1,3]
+ * 解释：2 与 6 之和等于目标数 8 。因此 index1 = 1, index2 = 3 。
+ *
+ * @Author: YangGC
+ * @Date: 06-11
+ */
+public class q006 {
+    public int[] twoSum(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low, high};
+            } else if (sum < target) {
+                ++low;
+            } else {
+                --high;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
