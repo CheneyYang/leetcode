6149cef1045ef760c37718a2add1b4cc67b94f33
yanggc <younggc@foxmail.com>
2022-06-13 20:59:07 +0800

    和大于等于 target 的最短子数组

---
+++ src/leetcode/ciof/q008.java
@@ -0,0 +1,43 @@
+package leetcode.ciof;
+
+/**
+ * 和大于等于 target 的最短子数组
+ * Description:
+ * <p>
+ * 给定一个含有n个正整数的数组和一个正整数 target 。
+ * <p>
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ * 示例 1：
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ *
+ * @Author: YangGC
+ * @Date: 06-13
+ */
+public class q008 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
