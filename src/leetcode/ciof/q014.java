7f76c79d5436b19574ca1b0271160e04363a1ad1
yanggc <younggc@foxmail.com>
2022-06-20 23:28:30 +0800

    字符串中的变位词

---
+++ src/leetcode/ciof/q014.java
@@ -0,0 +1,47 @@
+package leetcode.ciof;
+
+import java.util.Arrays;
+
+/**
+ *  字符串中的变位词
+ * Description:
+ * 给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的某个变位词。
+ * 换句话说，第一个字符串的排列之一是第二个字符串的 子串 。
+ *
+ * 示例 1：
+ * 输入: s1 = "ab" s2 = "eidbaooo"
+ * 输出: True
+ * 解释: s2 包含 s1 的排列之一 ("ba").
+ *
+ * @Author: YangGC
+ * @Date: 06-20
+ */
+public class q014 {
+
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if (n > m) { return false; }
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < n; ++i) {
+            ++cnt1[s1.charAt(i) - 'a'];
+            ++cnt2[s2.charAt(i) - 'a'];
+        }
+        if(Arrays.equals(cnt1,cnt2)){
+            return true;
+        }
+        for (int i = n; i < m; ++i) {
+            ++cnt2[s2.charAt(i) - 'a'];
+            --cnt2[s2.charAt(i - n) - 'a'];
+            if (Arrays.equals(cnt1, cnt2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
