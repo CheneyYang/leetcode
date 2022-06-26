fd739be9ac5f196f636295495df0c57c769fd735
yanggc <younggc@foxmail.com>
2022-06-27 01:35:54 +0800

    两个链表的第一个交叉点

---
+++ src/leetcode/ciof/q023.java
@@ -0,0 +1,80 @@
+package leetcode.ciof;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 两个链表的第一个重合节点
+ * <p>
+ * Description:
+ * 给定两个单链表的头节点headA 和 headB ，请找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * 图示两个链表在节点 c1 开始相交：
+ * 题目数据 保证 整个链式结构中不存在环。
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ *
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ *
+ *
+ * @Author: YangGC
+ * @Date: 06-27
+ */
+public class q023 {
+
+
+    /**
+     * 常规遍历
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        while (headA != null){
+            visited.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null){
+            if(visited.contains(headB)){
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public ListNode getNext() {
+            return next;
+        }
+
+        public void setNext(ListNode next) {
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
