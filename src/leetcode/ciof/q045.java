2ea8e05e28f77c1c923e275f66906fcd8ca6b7ac
YangGC <younggengchen@gmail.com>
2022-07-16 00:37:14 +0800

    二叉树最底层最左边的值

---
+++ src/leetcode/ciof/q045.java
@@ -0,0 +1,88 @@
+package leetcode.ciof;
+
+/**
+ * 二叉树最底层最左边的值
+ * <p>
+ * Description:
+ * 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
+ * 假设二叉树中至少有一个节点。
+ * 示例 1:
+ * 输入: root = [2,1,3]
+ * 输出: 1
+ * 输入: [1,2,3,4,null,5,6,null,null,7]
+ * 输出: 7
+ *
+ * @Author: YangGC
+ * DateTime: 07-16
+ */
+public class q045 {
+
+    int curVal = 0;
+    int curHeight = 0;
+
+    public int findBottomLeftValue(TreeNode root) {
+        int curHeight = 0;
+        dfs(root, 0);
+        return curVal;
+    }
+
+    public void dfs(TreeNode root, int height) {
+        if (root == null) {
+            return;
+        }
+
+        height++;
+        dfs(root.left, height);
+        dfs(root.right, height);
+        if (height > curHeight) {
+            curHeight = height;
+            curVal = root.val;
+        }
+
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+    }
+
+}
