8e16d9a16694b35db4a247250a9dabc1ab98d492
YangGC <younggengchen@gmail.com>
2022-07-20 01:30:54 +0800

    使用前缀和方法进行求解

---
+++ src/leetcode/ciof/q050.java
@@ -0,0 +1,91 @@
+package leetcode.ciof;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description:
+ * 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+ * <p>
+ * 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）
+ * 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ * 输出：3
+ * 解释：和等于 8 的路径有 3 条，如图所示。
+ *
+ * @Author: YangGC
+ * DateTime: 07-20
+ */
+public class q050 {
+
+    public int pathSum(TreeNode root, int targetSum) {
+        HashMap<Long, Integer> prefix = new HashMap<>();
+        prefix.put(0L, 1);
+        return dfs(root, prefix, 0, targetSum);
+    }
+
+    public int dfs(TreeNode root, Map<Long, Integer> prefix, long curr, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ret = 0;
+        curr += root.val;
+
+        ret = prefix.getOrDefault(curr - targetSum, 0);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);
+        ret += dfs(root.left, prefix, curr, targetSum);
+        ret += dfs(root.right, prefix, curr, targetSum);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) - 1);
+
+        return ret;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
