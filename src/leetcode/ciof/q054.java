37c691a8a1c9ef358c5866f8a0dc88cc9b985a52
YangGC <younggengchen@gmail.com>
2022-07-23 00:06:35 +0800

    所有大于等于节点的值之和

---
+++ src/leetcode/ciof/q054.java
@@ -0,0 +1,81 @@
+package leetcode.ciof;
+
+/**
+ * 所有大于等于节点的值之和
+ * Description:
+ * <p>
+ * 给定一个二叉搜索树，请将它的每个节点的值替换成树中大于或者等于该节点值的所有节点值之和。
+ * 提醒一下，二叉搜索树满足下列约束条件：
+ * 节点的左子树仅包含键 小于 节点键的节点。
+ * 节点的右子树仅包含键 大于 节点键的节点。
+ * 左右子树也必须是二叉搜索树。
+ * <p>
+ * 示例 1：
+ * 输入：root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+ * 输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+ *
+ * @Author: YangGC
+ * DateTime: 07-23
+ */
+public class q054 {
+
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
