678b75ac99d07a652d7acdcfc5d569ff52694723
YangGC <younggengchen@gmail.com>
2022-07-29 21:11:27 +0800

    出现频率最高的 k 个数字

---
+++ src/leetcode/ciof/q060.java
@@ -0,0 +1,57 @@
+package leetcode.ciof;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 出现频率最高的 k 个数字
+ * <p>
+ * Description:
+ * 给定一个整数数组 nums 和一个整数 k ，请返回其中出现频率前 k 高的元素。可以按 任意顺序 返回答案。
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ *
+ * @Author: YangGC
+ * DateTime: 07-29
+ */
+public class q060 {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            Integer num = entry.getKey();
+            Integer count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; i++) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
