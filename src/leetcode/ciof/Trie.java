22974dd05547d45c630b3762d7c0b4e29ceeddb5
YangGC <younggengchen@gmail.com>
2022-07-31 23:29:32 +0800

    q062

--- src/leetcode/ciof/Trie.java
+++ src/leetcode/ciof/Trie.java
@@ -12,19 +12,23 @@ public class Trie {
     private Trie[] children;
     private boolean isEnd;
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public Trie() {
         children = new Trie[26];
         isEnd = false;
     }
 
-    /** Inserts a word into the trie. */
+    /**
+     * Inserts a word into the trie.
+     */
     public void insert(String word) {
         Trie node = this;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            int index = ch -'a';
-            if(node.children[index] == null){
+            int index = ch - 'a';
+            if (node.children[index] == null) {
                 node.children[index] = new Trie();
             }
             node = node.children[index];
@@ -32,31 +36,34 @@ public class Trie {
         node.isEnd = true;
     }
 
-    /** Returns if the word is in the trie. */
+    /**
+     * Returns if the word is in the trie.
+     */
     public boolean search(String word) {
         Trie node = searchPrefix(word);
         return node != null && node.isEnd;
     }
 
-    /** Returns if there is any word in the trie that starts with the given prefix. */
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
     public boolean startsWith(String prefix) {
         return searchPrefix(prefix) != null;
     }
 
 
-    private  Trie searchPrefix(String prefix){
+    private Trie searchPrefix(String prefix) {
         Trie node = this;
         for (int i = 0; i < prefix.length(); i++) {
             char ch = prefix.charAt(i);
             int index = ch - 'a';
-            if(node.children[index] == null){
+            if (node.children[index] == null) {
                 return null;
             }
             node = node.children[index];
         }
         return node;
     }
-
 
 
     public static void main(String[] args) {
