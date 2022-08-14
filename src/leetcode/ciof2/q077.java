d7cb892133723a2c3ac4390fe3accc0c6973f043
YangGC <younggengchen@gmail.com>
2022-08-15 01:02:11 +0800

    链表排序

---
+++ src/leetcode/ciof2/q077.java
@@ -0,0 +1,84 @@
+package leetcode.ciof2;
+
+/**
+ * 链表排序:
+ * 并归排序
+ * <p>
+ * Description:
+ * 给定链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ * 输入：head = [4,2,1,3]
+ * 输出：[1,2,3,4]
+ *
+ * @Author: YangGC
+ * DateTime: 08-15
+ */
+public class q077 {
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    //节点排序
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        ListNode sorted = merge(list1, list2);
+        return sorted;
+    }
+
+    //合并两个链表
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
