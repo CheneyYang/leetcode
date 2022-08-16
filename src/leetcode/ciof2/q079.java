c9e40ba62e0d9bd7f7885ba7875828b144ace506
YangGC <younggengchen@gmail.com>
2022-08-16 23:57:20 +0800

    所有子集

---
+++ src/leetcode/ciof2/q079.java
@@ -0,0 +1,45 @@
+package leetcode.ciof2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 所有子集
+ * Description:
+ * 给定一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * <p>
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ *
+ * @Author: YangGC
+ * DateTime: 08-16
+ */
+public class q079 {
+
+    List<Integer> t = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            for (int i = 0; i < n; ++i) {
+                if ((mask & (1 << i)) != 0) {
+                    t.add(nums[i]);
+                }
+            }
+            ans.add(new ArrayList<Integer>(t));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
