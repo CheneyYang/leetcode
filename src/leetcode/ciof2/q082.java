b0a1f5f901afce39823ef7ad4de1028b32e26eb1
YangGC <younggengchen@gmail.com>
2022-08-19 23:43:49 +0800

    含有重复元素集合的组合

---
+++ src/leetcode/ciof2/q082.java
@@ -0,0 +1,70 @@
+package leetcode.ciof2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 含有重复元素集合的组合
+ * Description:
+ * 给定一个可能有重复数字的整数数组candidates和一个目标数target，找出candidates中所有可以使数字和为target的组合。
+ * candidates中的每个数字在每个组合中只能使用一次，解集不能包含重复的组合。
+ * <p>
+ * 示例1:
+ * 输入: candidates =[10,1,2,7,6,1,5], target = 8,
+ * 输出:
+ * [
+ * [1,1,6],
+ * [1,2,5],
+ * [1,7],
+ * [2,6]
+ * ]
+ *
+ * @Author: YangGC
+ * DateTime: 08-19
+ */
+public class q082 {
+    List<int[]> freq = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> sequence = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        for (int num : candidates) {
+            int size = freq.size();
+            if (freq.isEmpty() || num != freq.get(size - 1)[0]) {
+                freq.add(new int[]{num, 1});
+            } else {
+                ++freq.get(size - 1)[1];
+            }
+        }
+        dfs(0, target);
+        return ans;
+    }
+
+    public void dfs(int pos, int rest) {
+        if (rest == 0) {
+            ans.add(new ArrayList<Integer>(sequence));
+            return;
+        }
+        if (pos == freq.size() || rest < freq.get(pos)[0]) {
+            return;
+        }
+        dfs(pos + 1, rest);
+        int most = Math.min(rest / freq.get(pos)[0], freq.get(pos)[1]);
+        for (int i = 1; i <= most; ++i) {
+            sequence.add(freq.get(pos)[0]);
+            dfs(pos + 1, rest - i * freq.get(pos)[0]);
+        }
+        for (int i = 1; i <= most; ++i) {
+            sequence.remove(sequence.size() - 1);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
